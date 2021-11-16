5d0b37281246f1b010f07951eaf3dc725e27bf83
Lidia342 <lidiaabraham0@gmail.com>
2021-11-16 20:07:21 +0100

    Speech Recognition

--- app/src/main/java/com/example/smarthouse_android/Devices.java
+++ app/src/main/java/com/example/smarthouse_android/Devices.java
@@ -3,12 +3,16 @@ package com.example.smarthouse_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.speech.RecognizerIntent;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smarthouse_android.Model.DeviceModel;
 import com.example.smarthouse_android.Network.APIService;
@@ -19,6 +23,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,24 +34,10 @@ public class Devices extends AppCompatActivity {
 
     BufferedReader bufferedReader;
     PrintWriter printWriter;
-   /* TextView lamp;
-    TextView door;
-    TextView window;
-    ImageView lampOn;
-    ImageView lampOff;
-    ImageView doorOpen;
-    ImageView doorClosed;
-    ImageView windowOpen;
-    ImageView windowClosed;
-    TextView humidity;
-    TextView temp;
-    */
-
-
-
  static int temperature;
  static int humStatus;
 
+ String speak;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -63,21 +55,18 @@ public class Devices extends AppCompatActivity {
         TextView humidity = (TextView) findViewById(R.id.humidityText);
         TextView  temp = (TextView) findViewById(R.id.tempText);
         TextView window = (TextView) findViewById(R.id.windowOnOfff);
-        // lampOn = (ImageView) findViewById(R.id.lam);
         ImageView lampOff = (ImageView) findViewById(R.id.lampOff);
-        //doorOpen = (ImageView) findViewById(R.id.dooropen1);
         ImageView  doorClosed = (ImageView) findViewById(R.id.doorClosed);
-        //windowOpen = (ImageView) findViewById(R.id.windowopen1);
         ImageView   windowClosed = (ImageView) findViewById(R.id.windowClosed);
+        ImageButton speechRec = findViewById(R.id.speech);
 
-
+        speechRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySpeechRecognizer();
+            }
+        });
         getDeviceStatus();
-
-
-
-      /*  APIService api = RetrofitInstance.getRetrofitInstance().create(APIService.class);
-        Call<DeviceModel> call= api.getDevices();
-*/
 
         lampSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -97,7 +86,6 @@ public class Devices extends AppCompatActivity {
 
                         }
                     });
-
 
                     lamp.setText("LIGHT");
                     sendMessage( lamp.getText().toString());
@@ -215,6 +203,59 @@ public class Devices extends AppCompatActivity {
 
 
     }
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch  lampSwitch = findViewById(R.id.lightSwitch);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch   doorSwitch = findViewById(R.id.doorSwitch);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch    windowSwitch = findViewById(R.id.windowSwitch);
+        ImageButton speechRec = findViewById(R.id.speech);
+
+        if (requestCode != SPEECH_REQUEST_CODE || resultCode != RESULT_OK) {
+        }
+        else {
+
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+
+            String spokenText = results.get(0);
+
+            // Do something with spokenText
+            if (spokenText.equalsIgnoreCase("Light on")) {
+                lampSwitch.setChecked(true);
+            }
+            else if (spokenText.equalsIgnoreCase("Light off")) {
+                lampSwitch.setChecked(false);
+            }
+            else if(spokenText.equalsIgnoreCase("Door open")){
+                doorSwitch.setChecked(true);
+            }else if(spokenText.equalsIgnoreCase("Door close")) {
+                doorSwitch.setChecked(false);
+            }
+            else if(spokenText.equalsIgnoreCase("Window open")) {
+                windowSwitch.setChecked(true);
+            }
+            else if(spokenText.equalsIgnoreCase("Window close")) {
+                windowSwitch.setChecked(false);
+            }else{
+                Toast.makeText(Devices.this,"The say the correct commands",Toast.LENGTH_LONG).show();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
 
 
@@ -233,8 +274,6 @@ public class Devices extends AppCompatActivity {
 
                 if (response.isSuccessful()) {
                     DeviceModel deviceModel = (DeviceModel) response.body();
-                // temperature = deviceModel.getTemperature();
-                  //  humStatus = deviceModel.getHumidity();
                     temp.setText(String.valueOf(deviceModel.getTemperature()) +  "°C");
                     humidity.setText(String.valueOf(deviceModel.getHumidity())+ "%");
 
